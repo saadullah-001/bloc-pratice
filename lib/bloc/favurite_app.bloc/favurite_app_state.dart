117ca192a0ddd8cf06f0a8e3fc9e41b8f4092e66
SaadUllah <saadullah.developers@gmail.com>
2026-02-12 23:54:18 +0500

    Fav App v1

---
+++ lib/bloc/favurite_app.bloc/favurite_app_state.dart
@@ -0,0 +1,7 @@
+import 'package:equatable/equatable.dart';
+
+class FavuriteAppState extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
---
+++ lib/view/favurite_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class FavuriteApp extends StatefulWidget {
+  const FavuriteApp({super.key});
+
+  @override
+  State<FavuriteApp> createState() => _FavuriteAppState();
+}
+
+class _FavuriteAppState extends State<FavuriteApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
